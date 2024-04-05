42038c248c08ff815ea4708f216a73e0b4c04e98
Donovan Odom <dodomdev@gmail.com>
2024-04-05 14:39:37 -0700

    init

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const router  = require('./routes/router')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use(cors({
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200
+}))
+app.use('/', router)
+
+const port = 3001
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
